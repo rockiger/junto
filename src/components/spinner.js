bc427cfc60f1d54cebf63158f2d824731fd1088d
Marco Laspe <marco@rockiger.com>
2019-07-14 09:43:38 +0200

    rename attributes in circle element in Spinner class to match React's way of writing attributes

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -64,6 +64,6 @@ export default function Spinner() {
 
 function Svg() {
     return (<svg className="Spinner-spinner" width="32px" height="32px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-        <circle className="Spinner-path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+        <circle className="Spinner-path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
     </svg>)
 }
