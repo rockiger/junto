ba84c0b088a47f778f1d870a85810991b8fd2c10
Marco Laspe <marco@rockiger.com>
2019-07-15 10:58:15 +0200

    add unit test with enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,9 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 // Mock gapi
-global.window.gapi = { load: (name, callback) => {}}
+global.window.gapi = {
+    load: (name, callback) => { },
+};
+
+configure({ adapter: new Adapter() });
--- src/tests/nav.test.js
+++ src/tests/nav.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-
+import { shallow } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 
 import Nav from '../components/nav';
 
 describe('Nav', () => {
+    
     it('renders without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(
@@ -24,5 +25,19 @@ describe('Nav', () => {
         );
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
+    });
+
+    it('has no searchbar if not signed in', () => {
+        const element = shallow(
+            <Nav isSignedIn={ false } />
+        )
+        expect(element.find('.Nav-search-form').length).toBe(0); 
+    });
+
+    it('has a searchbar if signed in', () => {
+        const element = shallow(
+            <Nav isSignedIn={ true } />
+        )
+        expect(element.find('.Nav-search-form').length).toBe(1); 
     });
 });
