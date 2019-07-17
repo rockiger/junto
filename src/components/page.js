7816791047d68d7acb4daae049f3b3852dbaf7f4
Marco Laspe <marco@rockiger.com>
2019-07-17 08:17:03 +0200

    refactor editor into own component

--- src/components/page.js
+++ src/components/page.js
@@ -2,25 +2,23 @@
 /* global google */
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import { Beforeunload } from 'react-beforeunload';
 import { BrowserRouter as Router, Redirect } from 'react-router-dom';
-import { renameFile, downloadFile, getFileDescription, updateFile } from '../lib/gdrive';
+import { renameFile, downloadFile, getFileDescription } from '../lib/gdrive';
 
+import Editor from './editor';
 import Spinner from './spinner';
 
 import { API_KEY, UNTITLEDFILE, UNTITLEDNAME, EXT } from '../lib/constants';
 import { getTitleFromFileName } from '../lib/helper';
 
 export default class Page extends React.Component {
-    
+
     state = {
-            text: '',
-            fileId: this.props.match.params.id,
-            fileName: UNTITLEDFILE,
-            pageHead: UNTITLEDNAME,
-            fileLoaded: false,
+        editorDelta: {},
+        fileId: this.props.match.params.id,
+        fileName: UNTITLEDFILE,
+        pageHead: UNTITLEDNAME,
+        fileLoaded: false,
     }
 
     componentDidMount() {
@@ -31,56 +29,40 @@ export default class Page extends React.Component {
         // load editor content when user is signed in and can use drive api
         if (this.props.isSignedIn && !this.state.fileLoaded) {
             this.loadEditorContent();
-            gapi.load('picker', {'callback': () => console.log('Picker loaded')})
+            gapi.load('picker', { 'callback': () => console.log('Picker loaded') })
         }
 
         // when going from one page to the next, we check if the parmeter in the url changed
         if (prevProps.match.params.id !== this.props.match.params.id) {
             this.props.setGoToNewFile(false);
-                this.setState({ 
-                    fileId: this.props.match.params.id, 
-                    fileLoaded: false 
-                    },
-                    this.loadEditorContentsnow
+            this.setState({
+                fileId: this.props.match.params.id,
+                fileLoaded: false
+            },
+                this.loadEditorContent
             )
         }
     }
+    
+        loadEditorContent = async ev => {
+            if (this.state.fileId) {
+                const fileContent = await downloadFile(this.state.fileId);
+                const fileDescription = await getFileDescription(this.state.fileId);
+                const pageHead = getTitleFromFileName(fileDescription.name);
+                this.setState({
+                    editorDelta: fileContent ? JSON.parse(fileContent) : {},
+                    fileLoaded: true,
+                    fileName: fileDescription.name,
+                    pageHead,
+                });
+            } else {
+                Router.push('/');
+            }
+        }
 
-    componentWillUnmount() {
-        this.save();
+    setEditorDelta = (editorDelta) => {
+        this.setState({ editorDelta });
     }
-
-    handleChange = (value) => {
-        this.setState({ text: value })
-    }
-
-    onChange = debounce(
-        value => {
-            console.log('onChange');
-            if (!this.state.value) this.setState({ value: value });
-            localStorage.setItem(
-                this.state.fileId,
-                JSON.stringify({
-                    body: value(),
-                    date: new Date().toGMTString(),
-                }),
-            );
-        },
-        300,
-        false,
-    );
-
-    onSave = options => {
-        this.save();
-    };
-
-    onClickSave = () => {
-        this.save();
-    };
-
-    save = () => {
-        this.updateFileContent(this.state.fileId, this.state.text);
-    };
 
     onBlurInput = ev => {
         if (!this.state.pageHead) return;
@@ -95,50 +77,24 @@ export default class Page extends React.Component {
         this.setState({ pageHead: ev.target.value });
     };
 
-    loadEditorContent = async ev => {
-        if (this.state.fileId) {
-            const fileContent = await downloadFile(this.state.fileId);
-            const fileDescription = await getFileDescription(this.state.fileId);
-            const pageHead = getTitleFromFileName(fileDescription.name);
-            window.pageHead = pageHead;
-            this.setState({
-                text: fileContent,
-                fileLoaded: true,
-                fileName: fileDescription.name,
-                pageHead,
-            });
-        } else {
-            Router.push('/');
-        }
-    }
-
     openPicker = () => {
         const accessToken = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
         var view = new google.picker.View(google.picker.ViewId.DOCS);
         view.setMimeTypes("image/png,image/jpeg,image/jpg");
         var picker = new google.picker.PickerBuilder()
-              .addView(google.picker.ViewId.DOCS)
-              .setOAuthToken(accessToken)
-              .setDeveloperKey(API_KEY)
-              .setCallback((data) => console.log('DATA:', data))
-              .build();
+            .addView(google.picker.ViewId.DOCS)
+            .setOAuthToken(accessToken)
+            .setDeveloperKey(API_KEY)
+            .setCallback((data) => console.log('DATA:', data))
+            .build();
         picker.setVisible(true);
     }
 
-    updateFileContent = async (fileId, content) => {
-        try {
-            const response = await updateFile(fileId, content);
-            return response.result.id;
-        } catch (err) {}
-    }
-
     render() {
-        let editor = <ReactQuill 
-            bounds=".editorContainer"
-            onChange={this.handleChange} 
-            theme="snow"
-            // modules={ modules() }
-            value={this.state.text}  
+        let editor = <Editor
+            editorDelta={this.state.editorDelta}
+            fileId={this.state.fileId}
+            setEditorDelta={this.setEditorDelta}
         />;
         if (this.props.isSignedIn && this.props.match.params.id) {
             return (
@@ -185,46 +141,9 @@ export default class Page extends React.Component {
                         }
                         .editorInput:hover {
                             border-color: #dadce0;
-                        }
-                        #editorArea {
-                            padding-bottom: 2rem;
-                        }
-                        button:hover {
-                            background: unset;
-                            border: none;
-                        }
-                        .ql-snow.ql-toolbar button, .ql-snow .ql-toolbar button {
-                            min-width: unset;
-                        }
-                        .ql-toolbar.ql-snow {
-                            -webkit-align-items: center;
-                            align-items: center;
-                            border: none;
-                            display: -webkit-inline-box;
-                            display: -webkit-inline-flex;
-                            display: inline-flex;
-                            height: 51px;
-                            margin: 0;
-                            padding: 8px 2px;
-                            position: fixed;
-                            right: 1rem;
-                            top: 64px;
-                            white-space: nowrap;
-                            z-index: 1;
-                        }
-                        .ql-container.ql-snow {
-                            border: none;                            
-                        }
-                        .ql-editor {
-                            border-top: 1px solid var(--border-color);
-                            height: calc(100vh - 64px - 50px);
-                            overflow-y: auto;
-                            padding: 1rem .5rem;
-                        }
-                        
+                        }                        
                     `}</style>
                     {/* <button onClick={this.openPicker}>Open Picker</button>*/}
-                    <Beforeunload onBeforeunload={() => this.save()} />
                 </div>
             );
         } else if (!this.props.isSignedIn && this.props.isSigningIn)
@@ -240,43 +159,8 @@ Page.propTypes = {
     isSigningIn: PropTypes.bool.isRequired,
     match: PropTypes.shape({
         params: PropTypes.shape({
-          id: PropTypes.string.isRequired
+            id: PropTypes.string.isRequired
         })
-      }),
+    }),
     setGoToNewFile: PropTypes.func.isRequired,
-}
-
-function debounce(func, wait, immediate) {
-    var timeout;
-    return function() {
-        var context = this,
-            args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-}
-
-
-function modules() {
-    return (
-        {
-            'toolbar': [
-                [{ 'font': ['sofia', 'slabo', 'roboto', 'inconsolata', 'ubuntu'] }, { 'size': [] }],
-                [ 'bold', 'italic', 'underline', 'strike' ],
-                [{ 'color': [] }, { 'background': [] }],
-                [{ 'script': 'super' }, { 'script': 'sub' }],
-                [{ 'header': '1' }, { 'header': '2' }, 'blockquote', 'code-block' ],
-                [{ 'list': 'ordered' }, { 'list': 'bullet'}, { 'indent': '-1' }, { 'indent': '+1' }],
-                [ {'direction': 'rtl'}, { 'align': [] }],
-                [ 'link', 'image', 'video', 'formula' ],
-                [ 'clean' ]
-              ],
-        }
-    )
 }
