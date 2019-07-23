d0d170b7c3ef24e32db5cc64509beb6f3f88cddb
Marco Laspe <marco@rockiger.com>
2019-07-23 13:11:53 +0200

    style sidebar, app

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom'
 import PropTypes from 'prop-types';
 
 import { createFile, getFolderId } from '../lib/gdrive'
 import { UNTITLEDFILE } from '../lib/constants';
+import SidebarRenderer from './sidebarRenderer';
 
 export default class Sidebar extends React.Component {
     state = {
@@ -27,64 +27,13 @@ export default class Sidebar extends React.Component {
     }
 
     render() {
-        if (this.props.goToNewFile && this.state.fileId ) {
-            return <Redirect to={`/page/${this.state.fileId}`} />
-        }
-
         return (
-            <div className="Sidebar">
-                <div
-                    className="Sidebar-newButton"
-                    onClick={this.onClickNewButton}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" />
-                    </svg>
-                    <span>New Page</span>
-                </div>
-                <style>{`
-                    .Sidebar-newButton {
-                        box-shadow: 0 1px 2px 0 rgba(60, 64, 67, 0.302),
-                            0 1px 3px 1px rgba(60, 64, 67, 0.149);
-                        align-items: center;
-                        background-color: #fff;
-                        background-image: none;
-                        border: 1px solid transparent;
-                        border-radius: 24px;
-                        color: #3c4043;
-                        display: inline-flex;
-                        font-size: 14px;
-                        font-weight: 600;
-                        height: 48px;
-                        letter-spacing: 0.15px;
-                        line-height: 22px;
-                        margin: 0 0 0 -5px;
-                        min-width: 120px;
-                        padding: 0 10px 0 15px;
-                        text-transform: none;
-                        width: inherit;
-                        cursor: pointer;
-                    }
-                    .Sidebar-newButton:hover {
-                        box-shadow: 0 1px 3px 0 rgba(60, 64, 67, 0.302),
-                            0 4px 8px 3px rgba(60, 64, 67, 0.149);
-                        background-color: #f8f9fa;
-                        outline: none;
-                    }
-                    .Sidebar svg {
-                        fill: #3d5afe;
-                    }
-                    .Sidebar span {
-                        margin-left: 15px;
-                    }
-                `}</style>
-            </div>
-        );
+            <SidebarRenderer 
+                fileId={this.state.fileId}
+                goToNewFile={this.props.goToNewFile}
+                onClickNewButton={this.onClickNewButton}
+            />
+        )
     }
 }
 
---
+++ src/components/sidebarRenderer.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import { Redirect } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import { Fab } from '@material-ui/core';
+import AddIcon from 'mdi-react/AddIcon'
+
+
+const SidebarRenderer = props => {
+    const classes = useStyles();
+
+    if (props.goToNewFile && props.fileId ) {
+        return <Redirect to={`/page/${props.fileId}`} />
+    }
+
+    return (
+        <>
+            <Fab 
+                arial-label="Add"
+                className={classes.fab} 
+                color="secondary"
+                onClick={props.onClickNewButton}
+            >
+                <AddIcon className={classes.addIcon} />
+            </Fab>
+            <div className={classes.sidebar}>
+                <div
+                    className="Sidebar-newButton"
+                    onClick={props.onClickNewButton}
+                >
+                    <svg className={classes.svg}
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                    >
+                        <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" />
+                    </svg>
+                    New Page
+                </div>
+                <style>{`
+                    .Sidebar-newButton {
+                        box-shadow: 0 1px 2px 0 rgba(60, 64, 67, 0.302),
+                            0 1px 3px 1px rgba(60, 64, 67, 0.149);
+                        align-items: center;
+                        background-color: #fff;
+                        background-image: none;
+                        border: 1px solid transparent;
+                        border-radius: 24px;
+                        color: #3c4043;
+                        display: inline-flex;
+                        font-size: 14px;
+                        font-weight: 600;
+                        height: 48px;
+                        letter-spacing: 0.15px;
+                        line-height: 22px;
+                        margin: 0 0 0 -5px;
+                        min-width: 120px;
+                        padding: 0 1rem;
+                        text-transform: none;
+                        width: inherit;
+                        cursor: pointer;
+                    }
+                    .Sidebar-newButton:hover {
+                        box-shadow: 0 1px 3px 0 rgba(60, 64, 67, 0.302),
+                            0 4px 8px 3px rgba(60, 64, 67, 0.149);
+                        background-color: #f8f9fa;
+                        outline: none;
+                    }
+                `}</style>
+            </div>
+        </>
+    );
+}
+export default SidebarRenderer;
+
+function useStyles() {
+    const useStyles = makeStyles(theme => {
+        console.log(theme);
+
+        return ({
+            fab: {
+                color: '#fff',
+                position: 'fixed',
+                right: theme.spacing(3),
+                bottom: theme.spacing(3),
+                visibility: 'visible',
+                [theme.breakpoints.up('md')]: {
+                    display: 'none',
+                }
+            },
+            link: {
+                display: 'flex',
+                flexGrow: 1,
+                textDecoration: 'none',
+                alignItems: 'center',
+            },
+            listitem: {
+                padding: 0,
+                paddingRight: theme.spacing(2),
+            },
+            svg: {
+                fill: theme.palette.secondary.main,
+                paddingRight: theme.spacing(1),
+                width: theme.spacing(5),
+            }
+        })
+    })
+    return useStyles();
+}
