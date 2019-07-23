12db268e60d4159d835d51a1721c9da55f837e53
Marco Laspe <marco@rockiger.com>
2019-07-23 08:21:17 +0200

    move filelist to mui

--- src/components/fileList.js
+++ src/components/fileList.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-
-import Spinner from './spinner'
 import { listFiles, createFile, updateFile, getFolderId, createNewWiki } from '../lib/gdrive';
+import FileListRenderer from './fileListRenderer'
 import { EXT } from '../lib/constants';
-import { getTitleFromFileName, getExtFromFilenName } from '../lib/helper';
 
 export default class FileList extends React.Component {
     
@@ -38,70 +35,10 @@ export default class FileList extends React.Component {
 
     render() {
         return (
-            <div className="filelist">
-                <h1>Your work</h1>
-                <div className="filelist-tagline">Last edited</div>
-                {this.state.isLoading && <Spinner />}
-                {!this.state.isLoading && <ul className="filelist-list">
-                    {this.state.files
-                        .filter(file => {
-                            console.log('file.name:', file.name)
-                            const ext = getExtFromFilenName(file.name);
-                            console.log('ext:', ext);
-                            
-                            return (ext === EXT)
-                        })
-                        .map(file => {
-                            const filename = getTitleFromFileName(file.name);
-                            return (
-                                <li key={file.id}>
-                                    <Link
-                                        to={`/page/${file.id}`}
-                                    >
-                                        <img src="https://drive-thirdparty.googleusercontent.com/16/type/application/json" alt="Markdown file" /> <span>{filename}</span>
-                                    </Link>
-                                </li>
-                            );
-                        })}
-                </ul>}
-                <style>{`
-                        .filelist h1 {
-                            border-bottom: 1px solid var(--border-color);
-                            font-size: 1.5rem;
-                            font-weight: 400;
-                            margin: 0;
-                            padding: 1rem .5rem;
-                        }
-                        .filelist-list {
-                            list-style-type: none;
-                            padding-inline-start: unset;
-                        }
-                        .filelist-tagline {
-                            margin-top: 1rem;
-                            font-weight: 600;
-                        }
-                        .filelist-list a {
-                            border-radius: var(--border-radius);
-                            color: var(--link-color);
-                            display: block;
-                            font-size: 1rem;
-                            padding: .5rem 1rem;
-                        }
-                        .filelist-list a:hover {
-                            background-color: var(--hover-bg-color);
-                            text-decoration: none;
-                        }
-                        .filelist-list a img {
-                            height: 1rem;
-                            width: 1rem;
-                            padding-right: 1rem;
-                        }
-                        .filelist-list a img, .filelist-list a span {
-                            display: inline-block;
-                            vertical-align: middle;
-                        }
-                    `}</style>
-            </div>
+            <FileListRenderer 
+                isLoading={this.state.isLoading}
+                files={this.state.files}
+            />
         );
     }
 }
