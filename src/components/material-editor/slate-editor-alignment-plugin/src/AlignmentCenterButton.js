d64f162e7c47b6d5a8bdab1992c3f63d2610ecee
Marco Laspe <marco@rockiger.com>
2019-08-07 15:19:12 +0200

    add alignment buttons

--- src/components/material-editor/slate-editor-alignment-plugin/src/AlignmentCenterButton.js
+++ src/components/material-editor/slate-editor-alignment-plugin/src/AlignmentCenterButton.js
@@ -1,23 +1,37 @@
 import React from 'react'
 import classnames from 'classnames'
 import FontAwesome from 'react-fontawesome'
-import { Button } from '../../slate-editor-components/src'
 
+import FormatAlignCenterIcon from 'mdi-react/FormatAlignCenterIcon'
+
+import { Button } from '../../slate-editor-components/src'
+import { platform } from '../../slate-editor-utils/src'
 import { alignmentMarkStrategy, hasMark, getMark } from './AlignmentUtils'
 
-const AlignmentCenterButton = ({ value, onChange, changeState, className, style, type }) => (
-  <Button
-    style={style}
-    type={type}
-    onClick={e => onChange(alignmentMarkStrategy(value.change(), 'center'))}
-    className={classnames(
-      'slate-alignment-plugin--button',
-      { active: hasMark(value) && getMark(value).data.get('align') === 'center' },
-      className,
-    )}
-  >
-    <FontAwesome name="align-center" />
-  </Button>
-)
+const AlignmentCenterButton = ({
+    value,
+    onChange,
+    changeState,
+    outerState: { readOnly },
+    className,
+    style,
+    type,
+}) =>
+    readOnly ? null : (
+        <Button
+            active={
+                hasMark(value) && getMark(value).data.get('align') === 'center'
+            }
+            style={style}
+            type={type}
+            onClick={e =>
+                onChange(alignmentMarkStrategy(value.change(), 'center'))
+            }
+            className={classnames('slate-alignment-plugin--button', className)}
+            title={`Center Align (${platform.controlKey()}+Shift+C)`}
+        >
+            <FormatAlignCenterIcon />
+        </Button>
+    )
 
 export default AlignmentCenterButton
