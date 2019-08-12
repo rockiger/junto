91dfb67f1908f289928f02fc11583ceacd4f9df4
Marco Laspe <marco@rockiger.com>
2019-08-12 23:18:14 +0200

    basic drag and drop with images - not more not less

--- src/components/material-editor/slate-editor-image-plugin/src/ImageNode.js
+++ src/components/material-editor/slate-editor-image-plugin/src/ImageNode.js
@@ -1,76 +1,61 @@
 import React, { Component } from 'react'
 import classnames from 'classnames'
 
-import ImageDataModal from './ImageDataModal'
-import ImageEditLayer from './ImageEditLayer'
-
 // FIXME: Needs to handle assets files to work with SSR
 if (require('exenv').canUseDOM) require('./ImageNode.css')
 
 class ImageNode extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { isModalActive: false }
-  }
-
-  modal(isModalActive) {
-    if (isModalActive) {
-      const { editor: { onChange, props: { value } } } = this.props
-      onChange(value.change().select())
+    constructor(props) {
+        super(props)
+        this.state = { isModalActive: false }
     }
 
-    this.setState({ isModalActive })
-  }
+    modal(isModalActive) {
+        if (isModalActive) {
+            const {
+                editor: {
+                    onChange,
+                    props: { value },
+                },
+            } = this.props
+            onChange(value.change().select())
+        }
 
-  render() {
-    const { isModalActive } = this.state
-    const {
-      node,
-      attributes,
-      readOnly,
-      isSelected,
-      editor: {
-        onChange,
-        props: { value }
-      }
-    } = this.props
+        this.setState({ isModalActive })
+    }
 
-    return (
-      <span>
-        {!isModalActive ? null : (
-          <ImageDataModal
-            node={node}
-            value={value}
-            onChange={onChange}
-            changeModalState={this.modal.bind(this)}
-          />
-        )}
-
-        <div className={classnames('image-node--container', { readonly: readOnly })}>
-          {this.props.children}
-          {isSelected && (
-            <ImageEditLayer
-              changeModalState={this.modal.bind(this)}
-              text="Editar"
-            />
-          )}
-          {!readOnly && !isSelected && (
-            <ImageEditLayer
-              text="Selecione a imagem para editar"
-            />
-          )}
-          <img
-            {...attributes}
-            role="presentation"
-            className={`image-node ${!readOnly && isSelected && 'selected'}`}
-            src={node.data.get('src')}
-            title={node.data.get('title')}
-            alt={node.data.get('title')}
-          />
-        </div>
-      </span>
-    )
-  }
+    render() {
+        const { node, attributes, readOnly, isSelected } = this.props
+        console.log(this.props)
+        return (
+            <div
+                className={classnames('image-node--container', {
+                    readonly: readOnly,
+                })}
+            >
+                {this.props.children}
+                <img
+                    {...attributes}
+                    role="presentation"
+                    className={`image-node ${!readOnly &&
+                        isSelected &&
+                        'selected'}`}
+                    src={node.data.get('src')}
+                    title={node.data.get('title')}
+                    alt={node.data.get('title')}
+                    style={{
+                        display: 'block',
+                        maxWidth: '100%',
+                        maxHeight: '20em',
+                        boxShadow:
+                            isSelected && !readOnly
+                                ? '0 0 0 1px #4285f4'
+                                : 'none',
+                    }}
+                />
+            </div>
+        )
+    }
 }
 
 export default ImageNode
