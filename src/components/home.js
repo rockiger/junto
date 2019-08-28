ed92476e76a519504acc065cdf51230f828ddf02
Marco Laspe <marco@rockiger.com>
2019-08-29 00:38:57 +0200

    spinner position on home view

--- src/components/home.js
+++ src/components/home.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import FrontPageHero from './frontPageHero';
-import FileList from './fileList';
-import Spinner from './spinner';
+import FrontPageHero from './frontPageHero'
+import FileList from './fileList'
+import Spinner from './spinner'
 
 function Home(props) {
     if (props.isSignedIn && !props.isSigningIn) {
-        return <FileList />;
-    } else if ( !props.isSignedIn && props.isSigningIn) {
-        return <Spinner />;
+        return <FileList />
+    } else if (!props.isSignedIn && props.isSigningIn) {
+        return (
+            <div style={{ marginTop: '2rem' }}>
+                <Spinner />
+            </div>
+        )
     } else {
-        return <FrontPageHero />;
+        return <FrontPageHero />
     }
 }
 Home.propTypes = {
     isSignedIn: PropTypes.bool.isRequired,
     isSigningIn: PropTypes.bool.isRequired,
-};
+}
 
 export default Home
