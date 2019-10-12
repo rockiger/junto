9d27b84446e97899df037bce99d7b1d2da152d9a
Marco Laspe <marco@rockiger.com>
2019-10-13 00:14:34 +0200

    Update index.js: Setup reactn

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { setGlobal } from 'reactn'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import addReactNDevTools from 'reactn-devtools'
+addReactNDevTools()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+setGlobal({
+    isFileListLoading: false,
+    isSearchFieldActive: false,
+    oldSearchTerm: '',
+    redirect: false,
+    searchTerm: '',
+    files: [],
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 if (module.hot) {
-    module.hot.accept();
+    module.hot.accept()
 }
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
