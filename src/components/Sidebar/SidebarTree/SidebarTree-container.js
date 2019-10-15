f38a3618cdf3c2e3b1135da0ef2bf97a64e1dfab
Marco Laspe <marco@rockiger.com>
2019-10-15 08:07:42 +0200

    use gmail example of material treeview

--- src/components/Sidebar/SidebarTree/SidebarTree-container.js
+++ src/components/Sidebar/SidebarTree/SidebarTree-container.js
@@ -1,5 +1,25 @@
-import React from 'reactn'
+import React, { useGlobal } from 'reactn'
+
+import { SidebarTreeComponent } from './SidebarTree-component'
+
+import { OVERVIEW_NAME } from '../../../lib/constants'
 
 export const SidebarTree = props => {
-    return <div>SidebarTree</div>
+    const [rootFolderId] = useGlobal('rootFolderId')
+    const [files] = useGlobal('files')
+
+    console.log(filterOverview(files))
+    return (
+        <SidebarTreeComponent
+            rootFolderId={rootFolderId}
+            files={filterOverview(files)}
+        />
+    )
+}
+
+function filterOverview(files) {
+    return files.filter(file => {
+        if (file.name === OVERVIEW_NAME) return false
+        return true
+    })
 }
