bdc47ebe6cd3db1124ec6b795f5f5a8c075c8b79
Marco Laspe <marco@rockiger.com>
2019-10-19 17:04:50 +0200

    Update SidebarTree-component.js, SidebarTree-container.js, and fileListRenderer.js: Removed warnings.

--- src/components/Sidebar/SidebarTree/SidebarTree-container.js
+++ src/components/Sidebar/SidebarTree/SidebarTree-container.js
@@ -2,8 +2,6 @@ import React, { useGlobal } from 'reactn'
 
 import { SidebarTreeComponent } from './SidebarTree-component'
 
-import { OVERVIEW_NAME } from '../../../lib/constants'
-
 export const SidebarTree = props => {
     const [rootFolderId] = useGlobal('rootFolderId')
     const [initialFiles] = useGlobal('initialFiles')
