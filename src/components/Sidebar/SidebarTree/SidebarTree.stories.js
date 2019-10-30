d4844f509b7ec55b18e47821d417c82542ff5ab3
Marco Laspe <marco@rockiger.com>
2019-10-30 07:08:59 +0100

    Update SidebarTree.stories.js: Create a storybook item for SidebarTreeItem

--- src/components/Sidebar/SidebarTree/SidebarTree.stories.js
+++ src/components/Sidebar/SidebarTree/SidebarTree.stories.js
@@ -1,15 +1,14 @@
 import React, { setGlobal } from 'reactn'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { withKnobs, object } from '@storybook/addon-knobs/react'
 
-import { SidebarTree } from './SidebarTree-container'
-
+import { SidebarTreeItem } from './SidebarTreeItem'
+import { useStyles } from './SidebarTree-styles'
 import testState from './testState.js'
 
-setGlobal(testState)
-window.testState = testState
-console.log(testState)
+import { MYHOME } from '../../../lib/constants'
 
 storiesOf('SidebarTree', module)
     .addDecorator(withKnobs)
@@ -26,5 +25,22 @@ storiesOf('SidebarTree', module)
     .add('default', SidebarDefault)
 
 function SidebarDefault() {
-    return <SidebarTree />
+    const { files } = (window.testState = testState)
+    const classes = useStyles()
+
+    console.log(testState)
+    return (
+        <Router>
+            <ul className={classes.mydrive}>
+                <SidebarTreeItem
+                    expand={true}
+                    files={files}
+                    label={MYHOME}
+                    level={0}
+                    pageId={'1vYllhFQojUFl9PpVRnW1uqJcPGigXF2D'}
+                    parentId={'10t_Nrv_DUoOSbp9MoYGmfm1Cdj--Zc0D'}
+                />
+            </ul>
+        </Router>
+    )
 }
