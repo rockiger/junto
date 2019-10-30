84a971446ff4440429c853f4acdba97bcd838ae1
Marco Laspe <marco@rockiger.com>
2019-10-30 23:23:06 +0100

    Update App.test.js.snap, fileList.test.js.snap, and 3 more files...: Removed Snapshots, because we don't use them anymore

--- src/tests/fileList.test.js
+++ src/tests/fileList.test.js
@@ -1,28 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import React, { setGlobal } from 'reactn'
+import ReactDOM from 'react-dom'
+import { render, fireExent, cleanup } from '@testing-library/react'
 
-import FileList from '../components/fileList';
+import FileList from '../components/fileList'
 
-describe('FileList', () => {
-    const props = {
-        setIsSigningIn: (isSignedIn) => {},
-    };
+afterEach(cleanup)
+const date1 = new Date()
+const date2 = new Date()
+date2.setDate(date2.getDate() - 3)
+const date3 = new Date()
+date3.setDate(date3.getDate() - 15)
+const date4 = new Date()
+date4.setDate(date4.getDate() - 50)
+const date5 = new Date()
+date5.setDate(date4.getDate() - 1)
 
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(
-            <FileList />, div);
-        ReactDOM.unmountComponentAtNode(div);
-    });
+const files = [
+    { id: 'id1', name: 'File 1', modifiedByMeTime: date1.toISOString() },
+    { id: 'id2', name: 'File 2', modifiedByMeTime: date2.toISOString() },
+    { id: 'id3', name: 'File 3', modifiedByMeTime: date3.toISOString() },
+    { id: 'id4', name: 'File 4', modifiedByMeTime: date4.toISOString() },
+    { id: 'id5', name: 'File 5', modifiedByMeTime: date5.toISOString() },
+]
 
-    test('has a valid snapshot', () => {
-        const component = renderer.create(
-            <FileList />
-        );
-        const tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+setGlobal({
+    isCreatingNewFile: false,
+    rootFolderId: null,
+    isFileListLoading: false,
+    isSearchFieldActive: false,
+    isSignedIn: false,
+    isSigningIn: true,
+    goToNewFile: false,
+    oldSearchTerm: '',
+    redirect: false,
+    searchTerm: '',
+    searchValue: '', // The value in the searchfield
+    files: [],
+    initialFiles: [],
+    isInitialFileListLoading: false,
+    backgroundUpdate: false,
+})
 
-});
+it('Text in in headline', () => {
+    const { getByText, debug } = render(<FileList />)
+
+    expect(getByText(/Your Work/i).textContent).toBe('Your Work')
+    //eexpect(getByText(/Today/i).textContent).toBe('Today')
+})
