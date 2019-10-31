b62941609065ae7f7d5ab8fb77b1bc33d9848188
Marco Laspe <marco@rockiger.com>
2019-10-31 23:37:54 +0100

    Update cypress.json, example.json, and 9 more files...:Create first integration test with cypress

---
+++ cypress/integration/App-test.spec.js
@@ -0,0 +1,24 @@
+//import { GoogleSocialLogin } from 'cypress-social-logins/src/Plugins'
+
+describe('complete e to e test', () => {
+    it('Visit the homepage, login, watch a page and logout', () => {
+        cy.visit('/')
+        cy.contains('Knowledge Base for Google Drive!')
+        cy.contains('Login')
+        cy.contains('Login with Google').click()
+        cy.get('.Spinner').find('.Spinner-spinner')
+
+        cy.wait(1000)
+        cy.contains('New Page')
+        cy.contains('Your Work')
+        cy.contains('My Fulcrum')
+        cy.get('aside.App-sidebar ul > li > a')
+            .contains('My Fulcrum')
+            .click()
+
+        cy.wait(1000)
+        cy.contains('Welcome to your personal wiki')
+
+        cy.get('#LogoutButton').click()
+    })
+})
