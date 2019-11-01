99716b320ed3ebd870b3924c6e7f01777f7ca65a
Marco Laspe <marco@rockiger.com>
2019-11-01 12:38:54 +0100

    Update Editor-test.spec.js: Test - Toggle the editor via shortcuts

---
+++ cypress/integration/Editor-test.spec.js
@@ -0,0 +1,28 @@
+describe('Test the Editor', () => {
+    it('Toggle the editor via shortcuts', () => {
+        cy.visit('/')
+        cy.contains('Login with Google').click()
+
+        cy.wait(1000)
+
+        cy.get('aside.App-sidebar ul > li > a')
+            .contains('My Fulcrum')
+            .click()
+
+        cy.wait(1000)
+
+        cy.get('#SlateEditor').should('not.have.attr', 'contenteditable')
+        cy.get('body').type('{e}')
+
+        cy.wait(100)
+        cy.get('#SlateEditor').should('have.attr', 'contenteditable', 'true')
+
+        cy.wait(100)
+        cy.get('#SlateEditor').type('{ctrl}{enter}')
+        cy.wait(100)
+        cy.get('#SlateEditor').should('not.have.attr', 'contenteditable')
+
+        cy.wait(100)
+        cy.get('#LogoutButton').click()
+    })
+})
