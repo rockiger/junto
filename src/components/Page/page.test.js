0e8ef2bdf29dcc557acbfe48cbf66d3332762a7e
Marco Laspe <marco@rockiger.com>
2019-11-06 20:39:59 +0100

    Update App.js, index.js, and 3 more files...: Move page into it's own directory.

---
+++ src/components/Page/page.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+
+import { MemoryRouter } from 'react-router-dom'
+
+import Page from './page'
+
+describe('Page', () => {
+    const props = {
+        match: {
+            params: { id: '' },
+        },
+        setGoToNewFile: () => {},
+    }
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div')
+        ReactDOM.render(
+            <MemoryRouter>
+                <Page {...props} />
+            </MemoryRouter>,
+            div
+        )
+        ReactDOM.unmountComponentAtNode(div)
+    })
+})
