c8e238ed78c2673da761063e5127b5141c2db522
Marco Laspe <marco@rockiger.com>
2019-11-11 23:23:40 +0100

    Update SidebarTree-helper.js: Make it work, when there are shared files

--- src/components/Sidebar/SidebarTree/SidebarTree-helper.js
+++ src/components/Sidebar/SidebarTree/SidebarTree-helper.js
@@ -1,22 +1,56 @@
+//@ts-check
 import { EXT, OVERVIEW_NAME } from '../../../lib/constants'
 
+/**
+ *
+ * @param {string} fileId
+ * @param {any[]} files
+ * @returns {string | null}
+ */
 export function getFolderId(fileId, files) {
     const folder = files.find(file => file.name === fileId)
     return folder ? folder.id : null
 }
 
+/**
+ *
+ * @param {any[]} files
+ * @returns {string}
+ */
 export function getOverviewFileId(files) {
     const overview = files.find(file => file.name === OVERVIEW_NAME)
     if (overview) return overview.id
     return ''
 }
+
+/**
+ *
+ * @param {string} folderId
+ * @param {any[]} files
+ * @returns {any[]}
+ */
 export function filterChildFiles(folderId, files) {
-    if (folderId) return files.filter(file => file.parents.includes(folderId))
+    if (folderId)
+        return files.filter(file => {
+            try {
+                return file.parents && file.parents.includes(folderId)
+            } catch (err) {
+                console.error(err)
+                console.log(file)
+                return false
+            }
+        })
     return []
 }
 
+/**
+ *
+ * @param {object} file
+ * @param {string} parentId
+ */
 export function shouldFileDisplay(file, parentId) {
-    const { mimeType, name, parents, trashed } = file
+    const { mimeType, name, ownedByMe, parents, trashed } = file
+    if (!ownedByMe) return false
     return (
         mimeType === 'application/json' &&
         name !== OVERVIEW_NAME &&
