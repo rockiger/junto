04f1a253029d98c6ebdbd3a0a0522a079f39f043
Marco Laspe <marco@rockiger.com>
2020-01-25 15:08:33 +0100

    Update Footer.js: Show the right title again. Our Footer did our title in the wrong directtion.

--- src/components/staticPages/Footer.js
+++ src/components/staticPages/Footer.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles'
 // import { Link } from 'react-router-dom'
 
 export const Footer = () => {
-    document.title = 'Privacy Policy - Fulcrum.wiki'
     const styles = useStyles()
     return (
         <footer className={styles.footer}>
