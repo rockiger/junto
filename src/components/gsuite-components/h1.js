cd46031662d32e373fb9bd0301b765b545caf80c
Marco Laspe <marco@rockiger.com>
2020-06-23 01:01:46 +0200

    H1 component

---
+++ src/components/gsuite-components/h1.js
@@ -0,0 +1,27 @@
+//@ts-check
+import React from 'react'
+import clsx from 'clsx'
+
+export { H1 }
+
+export default function H1({ children, ...restProps }) {
+    return (
+        <>
+            <h1
+                className={clsx('gsuiteH1eurni', restProps.className)}
+                {...restProps}
+            >
+                {children}
+            </h1>
+            <style>{`
+                    .gsuiteH1eurni {
+                        border-bottom: 1px solid #dadce0;
+                        font-size: 1.5rem;
+                        font-weight: 400;
+                        margin: 0;
+                        padding: .5rem;
+                    }</>
+                `}</style>
+        </>
+    )
+}
