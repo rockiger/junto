a5b28b422b14994b3682e1e26fdc028a34890f6f
rockinger <marco.laspe@zippeo.io>
2021-05-23 22:53:58 +0200

    Let My Fulcrum standardly be closed, rename it to my wiki

--- cypress/integration/App-test.spec.js
+++ cypress/integration/App-test.spec.js
@@ -10,10 +10,8 @@ describe('complete e to e test', () => {
         cy.wait(1000)
         cy.contains('New Page')
         cy.contains('Your Work')
-        cy.contains('My Fulcrum')
-        cy.get('aside.App-sidebar ul > li > a')
-            .contains('My Fulcrum')
-            .click()
+        cy.contains('My Wiki')
+        cy.get('aside.App-sidebar ul > li > a').contains('My Wiki').click()
 
         cy.wait(1000)
         cy.contains('Welcome to your wiki')
@@ -22,7 +20,7 @@ describe('complete e to e test', () => {
         cy.get('#SearchAutocomplete__MenuList')
             .find('li')
             .first()
-            .contains('My Fulcrum')
+            .contains('My Wiki')
 
         cy.contains('Shared With Me').click()
         cy.wait(100)
