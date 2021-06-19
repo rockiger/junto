8a63a7eca9d36f3f789a593acfd887e57585b51d
rockinger <marco.laspe@zippeo.io>
2021-06-19 18:42:16 +0200

    Create 2 style helpers HStack and VStack

--- src/components/gsuite-components/style-helpers/style-helpers.js
+++ src/components/gsuite-components/style-helpers/style-helpers.js
@@ -3,8 +3,16 @@ import React from 'react'
 
 import s from './style-helpers.module.scss'
 
-export { Spacer }
+export { HStack, Spacer, VStack }
 
 function Spacer() {
     return <div className={s.Spacer}></div>
+}
+
+function HStack({ children }) {
+    return <div className={s.HStack}>{children}</div>
+}
+
+function VStack({ children }) {
+    return <div className={s.VStack}>{children}</div>
 }
